1e7c209d6e3a64944658d6e374cb4024f2514f8d
rookedsysc <rookedsysc36@gmail.com>
2024-10-10 14:59:33 +0900

    init

---
+++ src/main/kotlin/io/rookedsysc/securitycleanarchitecture/SecurityCleanArchitectureApplication.kt
@@ -0,0 +1,11 @@
+package io.rookedsysc.securitycleanarchitecture
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SecurityCleanArchitectureApplication
+
+fun main(args: Array<String>) {
+    runApplication<SecurityCleanArchitectureApplication>(*args)
+}
---
+++ src/test/kotlin/io/rookedsysc/securitycleanarchitecture/SecurityCleanArchitectureApplicationTests.kt
@@ -0,0 +1,13 @@
+package io.rookedsysc.securitycleanarchitecture
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SecurityCleanArchitectureApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
